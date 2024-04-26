51190d0150801062d510b816c4bb69e7029053e6
nandan033 <161374074+nandan033@users.noreply.github.com>
2024-04-26 13:37:10 +0530

    Update program2.js
    solution 2

--- program2.js
+++ program2.js
@@ -1,4 +1,44 @@
 function longestSubstring(s) {
+    function longestSubstring(s) {
+    
+        let n = s.length;
+
+        if (n === 0) return 0;
+
+
+        let maxlenth = 0;
+
+
+        const seen = {};
+
+
+        let start = 0;
+
+        M
+
+
+        for (let end = 0; end < n; end++) {
+
+
+            if (s[end] in seen && start <= seen[s[end]]) {
+
+                start = seen[s[end]] + 1;
+
+            }
+
+            else {
+
+                maxlenth - Math.max(maxlenth, end - start + 1);
+
+            }
+
+            seen[s[end]] - end
+
+        }
+
+        // Implementation of longestSubstring function
+
+        return maxlenth;
 
 
     // Implementation of longestSubstring function
